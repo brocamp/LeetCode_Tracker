8ae683663f5e0f1f7449005e1d74eba21b563b41
pranav <pranavofficial404@gmail.com>
2023-08-25 13:15:52 +0530

    basic structure for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
+import { multiselect } from "@clack/prompts";
 
+async function run() {
+	const additionalTools = await multiselect({
+		message: "Select additional tools.",
+		options: [
+			{ value: "eslint", label: "ESLint", hint: "recommended" },
+			{ value: "prettier", label: "Prettier" },
+			{ value: "gh-action", label: "GitHub Action" }
+		],
+		required: false
+	});
+}
+run();

19ec88b127369f5047e1794b9c46e963999002cf
pranav <106660600+prnv404@users.noreply.github.com>
2023-09-04 17:02:16 +0530

    whatsapp bot partially done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,50 @@
 import "dotenv/config";
 import "reflect-metadata";
-import { connectMongoDb } from "./config";
-import app from "./app";
+
+import qrcode from "qrcode-terminal";
+import { Events } from "whatsapp-web.js";
+
+import { client } from "./config";
+
 import * as cli from "./cli/ui";
+import app from "./app";
+
+import { connectMongoDb } from "./config";
 import { LeetcodeUpdaterTask } from "./handler/cronjob";
 
 const start = async () => {
 	cli.printIntro();
 
 	await connectMongoDb(process.env.MONGO_URI!);
+
+	client.on(Events.QR_RECEIVED, (qr: string) => {
+		qrcode.generate(qr, { small: true }, (qrcode: string) => {
+			cli.printQRCode(qrcode);
+		});
+	});
+
+	client.on(Events.LOADING_SCREEN, (percent: string) => {
+		if (percent == "0") {
+			cli.printLoading();
+		}
+	});
+
+	client.on(Events.AUTHENTICATED, () => {
+		cli.printAuthenticated();
+	});
+
+	// WhatsApp authentication failure
+	client.on(Events.AUTHENTICATION_FAILURE, () => {
+		cli.printAuthenticationFailure();
+	});
+
+	// WhatsApp ready
+	client.on(Events.READY, () => {
+		// Print outro
+		cli.printOutro();
+	});
+
+	await client.initialize();
 
 	LeetcodeUpdaterTask.start();
 

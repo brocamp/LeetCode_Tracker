1d23ac2e04178f56ed35eb26bb1b02c3e44ce54c
ajay <ajayzyan100@gmail.com>
2023-09-29 16:18:51 +0530

    chart loading issue resolved

--- frontend/src/components/LineChart.tsx
+++ frontend/src/components/LineChart.tsx
@@ -1,60 +1,65 @@
-
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { weeklyMetrics } from "../utils/api/config/axios.GetApi";
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
+
 export function LineChart() {
-	const date: any = [];
-	const setudentsCount: any = [];
-	useEffect(() => {
-		const getWeeklyMetrics = async () => {
-			const response: any = await weeklyMetrics();
-			response.data.lastWeekReport.map((dayObject: any) => {
-				date.push(dayObject.day);
-				setudentsCount.push(dayObject.totalStudentsSolved);
-			});
-		};
-		getWeeklyMetrics();
-	}, []);
+  const [date, setDate] = useState([]); 
+  const [studentsCount, setStudentsCount] = useState([]); 
 
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "top" as const
-			},
-			title: {
-				display: true,
-				text: "Sample chart"
-			}
-		}
-	};
+  useEffect(() => {
+    const getWeeklyMetrics = async () => {
+      const response:any = await weeklyMetrics();
+      const newDate:any = [];
+      const newStudentsCount:any = [];
 
-	const labels = date;
-	console.log(labels, "labels");
+      response.data.lastWeekReport.forEach((dayObject:any) => {
+        newDate.push(dayObject.day);
+        newStudentsCount.push(dayObject.totalStudentsSolved);
+      });
 
-	// Replace this array with your actual data
-	const rawData = setudentsCount;
-	console.log(rawData, "rowdta");
+      setDate(newDate);
+      setStudentsCount(newStudentsCount);
+    };
 
-	const data = {
-		labels,
-		datasets: [
-			{
-				fill: true,
-				label: "Dataset 2",
-				data: rawData,
-				borderColor: "rgb(53, 162, 235)",
-				backgroundColor: "rgba(53, 162, 235, 0.5)"
-			}
-		]
-	};
+    getWeeklyMetrics();
+  }, []);
 
-	return (
-		<>
-			<Line options={options} data={data} />
-		</>
-	);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Weekly analytics chart",
+      },
+    },
+  };
 
+  const labels = date;
+
+  // Replace this array with your actual data
+  const rawData = studentsCount;
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: "solved",
+        data: rawData,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Line options={options} data={data} />
+    </>
+  );
 }

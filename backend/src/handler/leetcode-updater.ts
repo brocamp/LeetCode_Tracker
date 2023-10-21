d56099a6c5fa3172805aff01368fc7125737b792
pranav <pranavofficial404@gmail.com>
2023-10-21 20:12:24 +0530

    Limit concurrency to 2 for performance improvement and bypass LeetCode rate limiting.

--- backend/src/handler/leetcode-updater.ts
+++ backend/src/handler/leetcode-updater.ts
@@ -3,6 +3,14 @@ import { Students, WeeklyMetrics } from "../database/model";
 import { StudentRepository } from "../database/repository";
 import { getProfile, getTotalSolved, getRecentSubmissionList } from "./leetcode";
 import { isToday, isAlreadySolvedOrNot, IsAlreadyInDb } from "./utils";
+import pLimit from "p-limit";
+
+
+/* This queue allows a maximum of 2 concurrent executions of the code block passed to it. It ensures that only 2
+students' profiles are updated at a time, preventing excessive resource usage and potential
+performance issues. */
+const queue = pLimit(2);
+
 /**
  * The LeetStudentProfileUpdate function updates the profiles of Leet students by retrieving their
  * LeetCode submissions, calculating their total solved count and recent submissions, and updating
@@ -16,52 +24,54 @@ export const LeetStudentProfileUpdate = async () => {
 	// Concurrency: Process students concurrently
 	await Promise.all(
 		students.map(async (student) => {
-			try {
-				// Getting leetcode profile
-				const leetcodeProfile = await getProfile(student.leetcodeId);
-				// Getting total questions solved by difficulty
-				const totalSubmissions = getTotalSolved(leetcodeProfile!)!;
-				// Getting recent array of  submission
-				const recentSubmissions = getRecentSubmissionList(leetcodeProfile!);
+			await queue(async () => {
+				try {
+					// Getting leetcode profile
+					const leetcodeProfile = await getProfile(student.leetcodeId);
+					// Getting total questions solved by difficulty
+					const totalSubmissions = getTotalSolved(leetcodeProfile!)!;
+					// Getting recent array of  submission
+					const recentSubmissions = getRecentSubmissionList(leetcodeProfile!);
 
-				student.solved = {
-					all: totalSubmissions.all,
-					easy: totalSubmissions.easy,
-					medium: totalSubmissions.medium,
-					hard: totalSubmissions.hard
-				};
-				// finding the recent submissionDate is today or not
-				student.lastSubmissionDate =
-					recentSubmissions?.find((problem) => {
+					student.solved = {
+						all: totalSubmissions.all,
+						easy: totalSubmissions.easy,
+						medium: totalSubmissions.medium,
+						hard: totalSubmissions.hard
+					};
+					// finding the recent submissionDate is today or not
+					student.lastSubmissionDate =
+						recentSubmissions?.find((problem) => {
+							return (
+								isToday(problem.timestamp) &&
+								problem.statusDisplay === "Accepted" &&
+								isAlreadySolvedOrNot(student.solvedQuestionsInThisWeek, problem.titleSlug)
+							);
+						})?.timestamp || student.lastSubmissionDate;
+
+					// if the last submisson date is not today will updating the totalNotSubmissionCount
+					if (isToday(student.lastSubmissionDate)) {
+						student.totalNotSubmissionCount = 0;
+					} else {
+						student.totalNotSubmissionCount++;
+					}
+
+					// filtering out total questions solved  today from the recentsubmission list and saving in the database
+					const solvedToday = recentSubmissions?.filter((submission) => {
 						return (
-							isToday(problem.timestamp) &&
-							problem.statusDisplay === "Accepted" &&
-							isAlreadySolvedOrNot(student.solvedQuestionsInThisWeek, problem.titleSlug)
+							submission.statusDisplay === "Accepted" &&
+							isToday(submission.timestamp) &&
+							IsAlreadyInDb(student.solvedQuestionsInThisWeek, submission.titleSlug)
 						);
-					})?.timestamp || student.lastSubmissionDate;
+					});
 
-				// if the last submisson date is not today will updating the totalNotSubmissionCount
-				if (isToday(student.lastSubmissionDate)) {
-					student.totalNotSubmissionCount = 0;
-				} else {
-					student.totalNotSubmissionCount++;
+					student.totalSolvedCountInThisWeek += solvedToday!.length;
+
+					await student.save();
+				} catch (error: any) {
+					logger.error(error.stack);
 				}
-
-				// filtering out total questions solved  today from the recentsubmission list and saving in the database
-				const solvedToday = recentSubmissions?.filter((submission) => {
-					return (
-						submission.statusDisplay === "Accepted" &&
-						isToday(submission.timestamp) &&
-						IsAlreadyInDb(student.solvedQuestionsInThisWeek, submission.titleSlug)
-					);
-				});
-
-				student.totalSolvedCountInThisWeek += solvedToday!.length;
-
-				await student.save();
-			} catch (error: any) {
-				logger.error(error.stack);
-			}
+			});
 		})
 	);
 
@@ -83,8 +93,10 @@ export const weeklyUpdate = async () => {
 
 	await Promise.all(
 		students.map(async (student) => {
-			student.totalSolvedCountInThisWeek = 0;
-			await student.save();
+			await queue(async () => {
+				student.totalSolvedCountInThisWeek = 0;
+				await student.save();
+			});
 		})
 	);
 };
